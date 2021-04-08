7df8c4acff96abca427e4056a639a8deb3dceb69
= <=>
2021-04-08 14:43:34 +0100

    return snap ik niet helemaal

--- 02-Javascript/taak03-Functions-Return/js/script.js
+++ 02-Javascript/taak03-Functions-Return/js/script.js
@@ -1,5 +1,44 @@
 function geefRandomGetalTerug(){                
-    
+let randomGetal = Math.random();  
+ randomGetal = randomGetal *10 + 1;
+ randomGetal = Math.floor(randomGetal);
+ return randomGetal;
 }
 
-console.log(geefRandomGetalTerug());    
+console.log(geefRandomGetalTerug());   
+
+  geefRandomGetalTerug()
+
+ console.log(geefRandomGetalTerug());
+
+ geefRandomGetalTerug()
+
+ console.log(geefRandomGetalTerug());
+
+ geefRandomGetalTerug()
+
+ console.log(geefRandomGetalTerug());
+
+ geefRandomGetalTerug()
+
+ console.log(geefRandomGetalTerug());
+
+ geefRandomGetalTerug()
+
+ console.log(geefRandomGetalTerug());
+
+ geefRandomGetalTerug()
+
+ console.log(geefRandomGetalTerug());
+
+ geefRandomGetalTerug()
+
+ console.log(geefRandomGetalTerug());
+
+ geefRandomGetalTerug()
+
+ console.log(geefRandomGetalTerug());
+
+ geefRandomGetalTerug()
+
+ console.log(geefRandomGetalTerug());
